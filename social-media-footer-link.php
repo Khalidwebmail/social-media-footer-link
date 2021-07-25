2c4ab4bf1aec86ed9ebcf521641d636d68930e2a
Ahmed Abdullah Khaled <csewebmail@gmail.com>
2021-07-25 19:57:53 +0600

    initial commit

---
+++ social-media-footer-link.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Social media footer link
+ *
+ * @package           PluginPackage
+ * @author            Khalid Ahmed
+ * @copyright         2019 Khalid Ahmed
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Social media footer link
+ * Plugin URI:        https://example.com/plugin-name
+ * Description:       Show author's facebook and linkedin url below his post
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Khalid Ahmed
+ * Author URI:        https://example.com
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       social-media-footer-link
+ * Domain Path:       /languages
+ */
+
+if( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Class Wp_Social_Media_Link
+ */
+
+final class Wp_Social_Media_Link {
+    /**
+     * Class constructor
+     */
+    public function __construct() {
+        
+    }
+}
